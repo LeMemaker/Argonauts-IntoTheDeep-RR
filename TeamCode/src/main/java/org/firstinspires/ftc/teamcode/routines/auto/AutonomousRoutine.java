891fe0998f2cfd6af21a94c4f590e1a352436720
YuzhuoMiao <144259729+YuzhuoMiao@users.noreply.github.com>
2025-01-22 12:37:20 -0500

    Update AutonomousRoutine.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/auto/AutonomousRoutine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/auto/AutonomousRoutine.java
@@ -3,18 +3,35 @@ package org.firstinspires.ftc.teamcode.routines.auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.TankDrive;
+
 import org.firstinspires.ftc.teamcode.routines.Routine;
 @Disabled
 @Autonomous(name = "Autonomous")
 public class AutonomousRoutine extends Routine {
+	public ViperSystem viperSystem;
+	public ClawSystem clawSystem;
+	public DriveSystem driveSystem;
 	@Override
 	public void onInit() {
 		super.onInit();
+		viperSystem = new ViperSystem(this);
+		clawSystem = new ClawSystem(this);
+		driveSystem = new DriveSystem(this);
+
+		clawSystem.set
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
+
 	}
 
 	@Override
