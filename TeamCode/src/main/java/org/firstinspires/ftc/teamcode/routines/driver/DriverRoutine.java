ae8a8517998943660ceb0c2c0a99449e834266a1
ZicanC <144257511+ZicanC@users.noreply.github.com>
2025-09-26 21:03:27 -0400

    Update DriverRoutine.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/driver/DriverRoutine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/routines/driver/DriverRoutine.java
@@ -10,11 +10,8 @@ import org.firstinspires.ftc.teamcode.subsystems.ViperSystem;
 
 @TeleOp(name = "Driver")
 public class DriverRoutine extends Routine {
-	public ViperSystem viperSystem;
-	public ClawSystem clawSystem;
 	public DriveSystem driveSystem;
 
-	private double threshold = 0.6;
 
 	@Override
 	public void onInit() {
@@ -27,8 +24,6 @@ public class DriverRoutine extends Routine {
 	@Override
 	public void onStart() {
 		super.onStart();
-		viperSystem = new ViperSystem(this);
-		clawSystem = new ClawSystem(this);
 		driveSystem = new DriveSystem(this);
 		try
 		{
@@ -38,22 +33,6 @@ public class DriverRoutine extends Routine {
 
 		}
 		while(opModeIsActive()){
-			viperSystem.moveOnTick(gamepad1.dpad_up, gamepad1.dpad_down);
-
-			viperSystem.holdDown(gamepad1.left_trigger == 1.0);
-
-
-			telemetry.addData("Left Viper Motor", viperSystem.getViper_motor_l().getCurrentPosition());
-			telemetry.addData("Right Viper Motor", viperSystem.getViper_motor_r().getCurrentPosition());
-
-			clawSystem.toggleClaw(gamepad1.right_bumper);
-			telemetry.addData("Left Servo Claw", clawSystem.getClaw_servo().getPosition());
-
-			clawSystem.shoulderOnTick(gamepad1.b, gamepad1.x);
-			telemetry.addData("Shoulder Position", clawSystem.getShoulder_servo_l().getPosition());
-
-			telemetry.addData("Shoulder ACTUAL Position", clawSystem.getActualPos());
-
 
 
 			driveSystem.driveOnTickVector(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
@@ -61,25 +40,7 @@ public class DriverRoutine extends Routine {
 			telemetry.addData("left_stick_x", gamepad1.left_stick_x);
 			telemetry.addData("left_stick_y", gamepad1.left_stick_x);
 			telemetry.addData("right_stick_x", gamepad1.right_stick_x);
-			telemetry.addData("Has Reached Position", clawSystem.isHasReached);
 
-
-
-			clawSystem.presetPosition(gamepad1.dpad_left, gamepad1.dpad_right, gamepad1.y, gamepad1.a);
-			if(!clawSystem.isHasReached) {
-				if (Math.abs(clawSystem.getShoulder_servo_l().getPosition() - clawSystem.getSetPos()) >= 0.005) {
-					clawSystem.setShoulderPos(clawSystem.getShoulder_servo_l().getPosition() + clawSystem.getDeltaPos());
-				} else {
-					clawSystem.isHasReached = true;
-				}
-			}
-
-			if((Math.abs(gamepad1.left_stick_x) > threshold || Math.abs(gamepad1.left_stick_y) > threshold || Math.abs(gamepad1.right_stick_x) > threshold)&& clawSystem.isOpen()){
-				clawSystem.setClawPos(0.0);//test
-			}// Maybe add this for vipers as well
-			else if(clawSystem.isOpen()){
-				clawSystem.setClawPos(0.55);
-			}
 
 			telemetry.update();
 		}
